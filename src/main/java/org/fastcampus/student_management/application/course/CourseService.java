65b1a5cf4c8e9fccca9fc100c6089a6e5c07f0be
Donghyun Lee <yahopet79@gmail.com>
2024-12-31 01:13:43 +0900

    Student Management

---
+++ src/main/java/org/fastcampus/student_management/application/course/CourseService.java
@@ -0,0 +1,39 @@
+package org.fastcampus.student_management.application.course;
+
+import java.util.List;
+import org.fastcampus.student_management.application.course.dto.CourseInfoDto;
+import org.fastcampus.student_management.application.student.StudentService;
+import org.fastcampus.student_management.domain.Course;
+import org.fastcampus.student_management.domain.CourseList;
+import org.fastcampus.student_management.domain.DayOfWeek;
+import org.fastcampus.student_management.domain.Student;
+import org.fastcampus.student_management.repo.CourseRepository;
+
+public class CourseService {
+
+    private final CourseRepository courseRepository;
+    private final StudentService studentService;
+
+    public CourseService(CourseRepository courseRepository, StudentService studentService) {
+        this.courseRepository = courseRepository;
+        this.studentService = studentService;
+    }
+
+    public void registerCourse(CourseInfoDto courseInfoDto) {
+        Student student = studentService.getStudent(courseInfoDto.getStudentName());
+        Course course = new Course(student, courseInfoDto.getCourseName(), courseInfoDto.getFee(),
+            courseInfoDto.getDayOfWeek(), courseInfoDto.getCourseTime());
+        courseRepository.save(course);
+    }
+
+    public List<CourseInfoDto> getCourseDayOfWeek(DayOfWeek dayOfWeek) {
+        List<Course> courses = courseRepository.getCourseDayOfWeek(dayOfWeek);
+        return courses.stream().map(CourseInfoDto::new).toList();
+    }
+
+    public void changeFee(String studentName, int fee) {
+        List<Course> courses = courseRepository.getCourseListByStudent(studentName);
+        CourseList courseList = new CourseList(courses);
+        courseList.changeAllCoursesFee(fee);
+    }
+}
---
+++ src/main/java/org/fastcampus/student_management/domain/Student.java
@@ -0,0 +1,52 @@
+package org.fastcampus.student_management.domain;
+
+public class Student {
+
+    private final String name;
+    private final int age;
+    private final String address;
+    private boolean activated;
+
+    public Student(String name, int age, String address) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("이름은 필수 입력값입니다.");
+        }
+
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.activated = true;
+    }
+
+    public void activate() {
+        if (this.activated) {
+            throw new IllegalArgumentException();
+        }
+        this.activated = true;
+    }
+
+    public void deactivate() {
+        if (!this.activated) {
+            throw new IllegalArgumentException();
+        }
+        this.activated = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public boolean isActivate() {
+        return activated;
+    }
+
+
+}
